74e42231d8419952eea3bcc31a2afe8396acbed7
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-05 20:01:17 -0700

    feat: add cuts and earnings to barber

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,7 +1,21 @@
-function createBarber(name) {
-  return {
-    name: name
+function createBarber(name, earnings, cuts) {
+  const barber = {
+    name: name,
+  };
+
+  if (earnings) {
+    barber.earnings = earnings;
+  };
+
+  if (cuts) {
+    barber.haircuts = [];
+
+    for (var i = 0; i < cuts.length; i++) {
+      barber.haircuts.push(cuts[i]);
+    };
   }
+
+  return barber
 }
 
 module.exports = {
