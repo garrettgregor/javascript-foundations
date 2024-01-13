216b92c39e59cd29cdc5c8e17fff9f817c3bc160
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-13 11:30:21 -0700

    feat: createSupplyCloset

--- crafting/crafts.js
+++ crafting/crafts.js
@@ -6,10 +6,31 @@ function createMaterial(name, unit, cost) {
   }
 }
 
+function calculateMaterialCost(material, quantity) {
+  var result = (material.costPerUnit * quantity);
+  
+  if(quantity > 1) {
+    return `${quantity} ${material.unit}s of ${material.name} costs $${result}.`;
+  } else {
+    return `${quantity} ${material.unit} of ${material.name} costs $${result}.`;
+  };
+}
+
+function createSupplyCloset(supplies) {
+  var results = { supplies: [] };
+
+  for(var i = 0; i < supplies.length; i++) {
+    results.supplies.push(supplies[i].name)
+  };
+
+  return results;
+
+}
+
 module.exports = {
   createMaterial,
-  // calculateMaterialCost,
-  // createSupplyCloset,
+  calculateMaterialCost,
+  createSupplyCloset,
   // addSupply,
   // createNewProject,
   // compareMaterials
