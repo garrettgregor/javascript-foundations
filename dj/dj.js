736ca0aa89411857371231fa98daf95ff6947a2f
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-15 18:07:40 -0700

    feat: dj createSong, playSong, makePlaylist, addSongToPlaylist and playSongs

--- dj/dj.js
+++ dj/dj.js
@@ -1,8 +1,65 @@
+function createSong(name, artist, favorite) {
+  const song = {
+    name: name,
+    artist: artist,
+    hasBeenPlayed: false,
+  };
 
-module.exports = { 
-  // createSong, 
-  // playSong, 
-  // makePlaylist, 
-  // addSongToPlaylist, 
-  // playSongs
+  if(favorite) {
+    song.favorite = favorite;
+  } else {
+    song.favorite = false
+  }
+
+  return song;
+};
+
+function playSong(song) {
+  song.hasBeenPlayed = true;
+
+  return song;
+}
+
+function makePlaylist(name, songs) {
+  var playlist = {
+    name: name,
+    songs: []
+  };
+
+  for(var i = 0; i < songs.length; i++) {
+    playlist.songs.push(songs[i]);
+  };
+
+  return playlist;
+};
+
+function addSongToPlaylist(playlist, song) {
+  playlist.songs.push(song);
+
+  return playlist;
+};
+
+function playSongs(playlist, option) {
+  if(option === "favorites only") {
+    for(var i = 0; i < playlist.songs.length; i++) {
+      if(playlist.songs[i].favorite === true) {
+        playSong(playlist.songs[i]);
+      }
+    };
+  } else {
+    for(var i = 0; i < playlist.songs.length; i++) {
+      playSong(playlist.songs[i]);
+    };
+  }
+
+  return playlist
+}
+
+
+module.exports = {
+  createSong,
+  playSong,
+  makePlaylist,
+  addSongToPlaylist,
+  playSongs
 };
