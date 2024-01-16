027b13bb819718898cbfaeff2fa69f9142dd8fa1
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-15 18:15:36 -0700

    feat: celebrateBirthday, countBirthdays

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -6,6 +6,24 @@ function createBirthday(name, month, day) {
   };
 }
 
+function celebrateBirthday(birthday) {
+  return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+};
+
+function countBirthdays(birthdays, monthNum) {
+  var count = 0
+
+  for(var i = 0; i < birthdays.length; i++) {
+    if(birthdays[i].month === monthNum) {
+      count += 1
+    };
+  };
+
+  return count;
+}
+
 module.exports = {
   createBirthday,
- };
+  celebrateBirthday,
+  countBirthdays
+};
