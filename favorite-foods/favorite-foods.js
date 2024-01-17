035bead1c000fec9be2cafcd716e0464e21d4be8
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-17 16:38:17 -0700

    feat: createFavoriteFood, commentOnSpiciness, orderFood, createShoppingList

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,43 @@
+function createFavoriteFood(object) {
+  return {
+    name: object.dish,
+    ingredients: object.ingredients,
+    isSpicy: object.isSpicy,
+    timesOrdered: 0
+  };
+};
+
+function commentOnSpiciness(dish) {
+  if (dish.isSpicy === true) {
+    return `Wow, this ${dish.name} is so spicy!`;
+  } else {
+    return `Phew, this ${dish.name} is not very spicy.`;
+  };
+};
+
+function orderFood(dish) {
+  dish.timesOrdered += 1;
+  return dish;
+};
+
+function createShoppingList(dishes) {
+  let results = []
+
+  for (var i = 0; i < dishes.length; i++) {
+    for (var j = 0; j < dishes[i].ingredients.length; j++) {
+      if (!results.includes(dishes[i].ingredients[j])) {
+        results.push(dishes[i].ingredients[j]);
+      }
+    }
+  };
+
+  return results;
+}
 
 
-
-module.exports = { };
+module.exports = {
+  createFavoriteFood,
+  commentOnSpiciness,
+  orderFood,
+  createShoppingList
+};
