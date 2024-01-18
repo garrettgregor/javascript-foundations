4f94ea6caeabb309c645066837043f2470ea925e
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-18 01:39:06 -0700

    test: issueCard, searchByAuthor

--- library/library-test.js
+++ library/library-test.js
@@ -2,8 +2,8 @@ var { issueCard, searchByAuthor } = require('./library');
 var assert = require('chai').assert;
 
 describe('Library', function() {
-  
-  it.skip('should issue a library card', function() {
+
+  it('should issue a library card', function() {
     var card = issueCard('George', 32);
 
     assert.equal(card.name, 'George');
@@ -11,7 +11,7 @@ describe('Library', function() {
     assert.equal(card.numBooksCheckedOut, 0);
   });
 
-  it.skip('should issue a card and check if it belongs to a child younger than 12 years old', function() {
+  it('should issue a card and check if it belongs to a child younger than 12 years old', function() {
     var card = issueCard('George', 32);
 
     assert.equal(card.name, 'George');
@@ -27,26 +27,26 @@ describe('Library', function() {
     assert.equal(childCard.isChild, true);
   });
 
-  it.skip('should be able to search through a catalog by author and return book result', function() {
+  it('should be able to search through a catalog by author and return book result', function() {
     var collection = [
       {title: 'My Life', author: 'Alex', status: 'on shelf'},
       {title: 'Adventures', author: 'Sam', status: 'checked out'},
       {title: 'Cooking Food', author: 'Jamie', status: 'on shelf'}
     ]
-    
+
     var searchResults = searchByAuthor(collection, 'Sam');
 
     assert.deepEqual(searchResults, [{title: 'Adventures', author: 'Sam', status: 'checked out'}]);
   });
 
-  it.skip('should be able to handle a search with multiple results', function() {
+  it('should be able to handle a search with multiple results', function() {
     var collection = [
       {title: 'My Life', author: 'Alex', status: 'on shelf'},
       {title: 'Adventures', author: 'Sam', status: 'checked out'},
       {title: 'Cooking Food', author: 'Jamie', status: 'on shelf'},
       {title: 'Cooking Spicy Things', author: 'Jamie', status: 'on shelf'}
     ]
-    
+
     var searchResult = searchByAuthor(collection, 'Jamie');
 
     assert.deepEqual(searchResult, [
@@ -55,13 +55,13 @@ describe('Library', function() {
     ]);
   });
 
-  it.skip('should be able to handle a search with no results', function() {
+  it('should be able to handle a search with no results', function() {
     var collection = [
       {title: 'My Life', author: 'Alex', status: 'on shelf'},
       {title: 'Adventures', author: 'Sam', status: 'checked out'},
       {title: 'Cooking Things', author: 'Jamie', status: 'on shelf'}
     ]
-    
+
     var searchResult = searchByAuthor(collection, 'Vicky');
 
     assert.equal(searchResult, 'No book found for search criteria');
