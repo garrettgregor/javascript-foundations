fa55e757d5f6b813543aa82cff6e7523f5180ed8
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-18 01:39:11 -0700

    feat: issueCard, searchByAuthor

--- library/library.js
+++ library/library.js
@@ -1,3 +1,29 @@
+function issueCard(name, age) {
+  return {
+    name: name,
+    age: age,
+    numBooksCheckedOut: 0,
+    isChild: age < 18
+  };
+};
 
+function searchByAuthor(collection, authorName) {
+  let results = []
 
-module.exports = { };
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i].author === authorName) {
+      results.push(collection[i]);
+    }
+  };
+
+  if (results.length === 0) {
+    return 'No book found for search criteria';
+  } else {
+    return results;
+  };
+}
+
+module.exports = {
+  issueCard,
+  searchByAuthor,
+};
