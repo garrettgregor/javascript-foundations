ada3748f2c753b0a90791cfd11bdf512464d5b79
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-19 21:07:39 -0700

    feat: createMeal, addDish, calculateCalories

--- meal-planning/mealPlanning.js
+++ meal-planning/mealPlanning.js
@@ -1,2 +1,32 @@
+function createMeal(type, numOfCalories) {
+  return {
+    type: type,
+    calorieGoal: numOfCalories,
+    dishes: []
+  }
+};
 
-module.exports = { }
+function addDish(meal,dish) {
+  if (dish.calories <= meal.calorieGoal) {
+    meal.calorieGoal -= dish.calories;
+    meal.dishes.push(dish);
+  };
+
+  return meal;
+};
+
+function calculateCalories(meal) {
+  count = 0;
+
+  for (var i = 0; i < meal.dishes.length; i++) {
+    count += meal.dishes[i].calories;
+  };
+
+  return `${meal.type} has a total of ${count} calories.`;
+}
+
+module.exports = {
+  createMeal,
+  addDish,
+  calculateCalories
+}
