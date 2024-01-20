443d9decba1fdcd4700a0c00e0a5966903f54b3b
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-20 11:58:34 -0700

    feat: createDragon, greetRider, eat, findFireBreathers

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,8 +1,51 @@
+function createDragon(name, rider, temperment) {
+  let dragon = {
+    name: name,
+    timesEaten: 0,
+    hungry: true
+  };
 
+  if (rider) {
+    dragon.rider = rider;
+  };
+
+  if (temperment) {
+    dragon.temperment = temperment;
+  };
+
+  return dragon;
+};
+
+function greetRider(dragon) {
+  return `Hi, ${dragon.rider}!`
+};
+
+function eat(dragon) {
+  dragon.timesEaten += 1
+
+  if (dragon.timesEaten >= 3) {
+    dragon.timesEaten = 3;
+    dragon.hungry = false;
+  };
+
+  return dragon;
+};
+
+function findFireBreathers(dragons) {
+  let results = []
+
+  for (var i = 0; i < dragons.length; i++) {
+    if (dragons[i].temperment === 'aggressive') {
+      results.push(dragons[i]);
+    };
+  };
+
+  return results;
+}
 
 module.exports = {
-  // createDragon, 
-  // greetRider, 
-  // eat, 
-  // findFireBreathers
+  createDragon,
+  greetRider,
+  eat,
+  findFireBreathers
 }
