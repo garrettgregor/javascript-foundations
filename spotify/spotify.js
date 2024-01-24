d982b2088d0b77842854f24eb71a489291e4425a
Garrett Gregor <garrett.gregor@gmail.com>
2024-01-24 12:42:22 -0700

    feat: createCollection, createTrack, reviewTrack, addTrack, getTotalDuration, findTracksByArtist

--- spotify/spotify.js
+++ spotify/spotify.js
@@ -1,4 +1,66 @@
+function createCollection(name, description) {
+  var collection = {
+    name: name,
+    description: description,
+    tracks: []
+  };
 
+  return collection;
+};
 
+function createTrack(title = 'unknown', artist = 'unknown', duration = 0) {
+  return track = {
+    title,
+    artist,
+    duration
+  };
+};
 
-  module.exports = {  }
+function reviewTrack(track) {
+    if (track.artist === 'Red Hot Chili Peppers') {
+      return `The song ${track.title} rules!`
+    } else {
+      return `I wish this was a Red Hot Chili Peppers song.`
+    };
+
+  // using a ternary operator instead
+  // return track.artist === 'Red Hot Chili Peppers'
+  //   ? `The song ${track.title} rules!`
+  //   : `I wish this was a Red Hot Chili Peppers song.`;
+};
+
+function addTrack(collection, track) {
+  collection.tracks.push(track);
+  return collection;
+};
+
+function getTotalDuration(collection) {
+  count = 0
+
+  for (var i = 0; i < collection.tracks.length; i++) {
+    count += collection.tracks[i].duration;
+  }
+
+  return count;
+};
+
+function findTracksByArtist(collection, query) {
+  results = []
+
+  for (var i = 0; i < collection.tracks.length; i++) {
+    if (collection.tracks[i].artist === query) {
+      results.push(collection.tracks[i]);
+    };
+  };
+
+  return results;
+};
+
+module.exports = {
+  createCollection,
+  createTrack,
+  reviewTrack,
+  addTrack,
+  getTotalDuration,
+  findTracksByArtist
+}
